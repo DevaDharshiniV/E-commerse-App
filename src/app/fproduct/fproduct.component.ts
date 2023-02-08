9896bd807c4efaa9042d40191611e4553ba991fd
DevaDharshiniV <devadharshiniv.it2020@citchennai.net>
2023-02-08 14:16:38 +0530

    commit

--- src/app/fproduct/fproduct.component.ts
+++ src/app/fproduct/fproduct.component.ts
@@ -9,43 +9,27 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./fproduct.component.css']
 })
 export class FproductComponent implements OnInit {
+  sizes=['S', 'M', 'L', 'XL'];
   product;
   addedToCart = false;
   message = "Added to Cart!";
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
-
-  // ngOnInit() {
-  //   const productId = 1; // This should come from somewhere, e.g. a route parameter
-  //   this.product = this.productService.getProductById(productId);
-  // }
   selectedSize: string;
   ngOnInit() {
     this.route.params.subscribe(params => {
       const productId = +params['id'];
       console.log('productId: ', productId);
       this.product = this.productService.getProductById(productId);
+      this.selectedSize = this.product.sizes[0];
     });
-    this.selectedSize = this.product.sizes[0];
+
   }
-  // ngOnInit() {
-  //   this.route.params.subscribe(params => {
-  //     if (params['id']) {
-  //       const productId = +params['id'];
-  //       console.log('productId: ', productId);
-  //       this.product = this.productService.getProductById(productId);
-  //     } else if (params['kid']) {
-  //       const productKid = +params['kid'];
-  //       console.log('productKid: ', productKid);
-  //       // this.product = this.productService.getProductByKid(productKid);
-  //     }
-  //   });
-  // }
   addToCart() {
     this.addedToCart = true;
     const cartItem = {
       fname: this.product.fname,
-      description: this.product.description,
+      size: this.selectedSize,
       price: this.product.price,
       image: this.product.image
     };
